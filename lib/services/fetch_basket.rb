e71adc96aa4841f3720a1fc21e39faa9ecfcf9b2
agatasalska <agata.salska@gmail.com>
2016-07-11 17:27:00 +0200

    Change fetch_basket.rb service file

--- lib/services/fetch_basket.rb
+++ lib/services/fetch_basket.rb
@@ -1,8 +1,12 @@
 module Shop
   class FetchBasket
-    def call(id)
-      BASKET.find do |basket|
-        basket.id == id
+    def call
+      BASKET.map do |b|
+        {
+          product: b.fetch_product,
+          quantity: b.quantity,
+          total_price: b.total_price
+        }
       end
     end
   end
